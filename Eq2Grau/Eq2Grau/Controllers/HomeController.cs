6be10a932cccbaa24e2d6eb8e882788aa9745375
Jose Casimiro Pereira <casimiro@ipt.pt>
2024-03-05 17:00:34 +0000

    Commit inicial

---
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Eq2Grau.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System.Diagnostics;
+
+namespace Eq2Grau.Controllers {
+
+    public class HomeController : Controller {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index(string A, string B, string C) {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
