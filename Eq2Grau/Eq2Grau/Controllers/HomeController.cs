9d9a433826e68aef00a0cc401a6df9cf0df8d123
Jose Casimiro Pereira <casimiro@ipt.pt>
2024-03-12 09:29:55 +0000

    construção do algoritmo de cálculo das raízes

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -24,32 +24,118 @@ namespace Eq2Grau.Controllers {
         /// <param name="C">parâmetro independente</param>
         /// <returns></returns>
         public IActionResult Index(string A, string B, string C) {
-            /* ALGORITMO:
-             * 1- ler parâmetros A, B, C
-             * 2- validar se se pode fazer operações com eles
-             *    2.1- têm de ser números
-             *    2.2- A != 0
-             * 3- se posso calcular,
-             *    3.1- determinar as raízes
-             *         x=(-b +/- sqrt(b^2-4ac))/2/a
-             *         3.1.1- calcular o delta: b^2-4ac
-             *         3.1.2- se delta > 0, há 2 raízes reais distintas
-             *                              x1 e x2
-             *                se delta = 0, há 2 raízes reais iguais
-             *                              x1=x2
-             *                se delta < 0, há duas raízes complexas conjugadas
-             *                              x1 = -b/(2a) + sqrt(-delta)/(2a) i
-             *                              x2 = -b/(2a) - sqrt(-delta)/(2a) i
-             *   3.2- enviar a resposta para o cliente
-             * se não posso calcular (else)
-             *    enviar mensagem de erro para o cliente (browser)
-             */
+         /* ALGORITMO:
+          * 1- ler parâmetros A, B, C
+          * 2- validar se se pode fazer operações com eles
+          *    2.1- têm de ser números
+          *    2.2- A != 0
+          * 3- se posso calcular,
+          *    3.1- determinar as raízes
+          *         x=(-b +/- sqrt(b^2-4ac))/2/a
+          *         3.1.1- calcular o delta: b^2-4ac
+          *         3.1.2- se delta > 0, há 2 raízes reais distintas
+          *                              x1 e x2
+          *                se delta = 0, há 2 raízes reais iguais
+          *                              x1=x2
+          *                se delta < 0, há duas raízes complexas conjugadas
+          *                              x1 = -b/(2a) + sqrt(-delta)/(2a) i
+          *                              x2 = -b/(2a) - sqrt(-delta)/(2a) i
+          *   3.2- enviar a resposta para o cliente
+          * se não posso calcular (else)
+          *    enviar mensagem de erro para o cliente (browser)
+          */
+
+         // vars. auxiliares
+         double auxA = 0, auxB = 0, auxC = 0;
 
 
+         // 1.
+         if (string.IsNullOrWhiteSpace(A) || string.IsNullOrWhiteSpace(B) || string.IsNullOrWhiteSpace(C)) {
+            // enviar mensagem para o utilizador
 
 
-
+            // devolver controlo à View
             return View();
+         }
+
+         // 1.
+         if (!double.TryParse(A, out auxA)) {
+            // o A não é número.
+            // enviar mensagem para o utilizador
+
+
+            // devolver controlo à View
+            return View();
+         }
+
+         // 1.
+         if (!double.TryParse(B, out auxB)) {
+            // o B não é número.
+            // enviar mensagem para o utilizador
+
+
+            // devolver controlo à View
+            return View();
+         }
+
+         // 1.
+         if (!double.TryParse(C, out auxC)) {
+            // o C não é número.
+            // enviar mensagem para o utilizador
+
+
+            // devolver controlo à View
+            return View();
+         }
+
+
+         // 2.
+         if (auxA == 0) {
+            // o A é ZERO.
+            // enviar mensagem para o utilizador
+
+
+            // devolver controlo à View
+            return View();
+         }
+
+
+         // 3.
+         double delta = Math.Pow(auxB, 2) - 4 * auxC;
+         // 3.1
+         if (delta > 0) {
+            string x1 = Math.Round((-auxB + Math.Sqrt(delta)) / 2 / auxA, 2) + "";
+            string x2 = Math.Round((-auxB - Math.Sqrt(delta)) / 2 / auxA, 2) + "";
+            // enviar mensagem para o utilizador
+
+
+            // devolver controlo à View
+            return View();
+         }
+
+         if (delta == 0) {
+            string x = Math.Round(-auxB / 2 / auxA, 2) + "";
+
+            // enviar mensagem para o utilizador
+
+
+            // devolver controlo à View
+            return View();
+         }
+
+         if (delta < 0) {
+            string x1 = Math.Round((-auxB) / 2 / auxA, 2) + " + " + Math.Round(Math.Sqrt(-delta) / 2 / auxA, 2) + " i";
+            string x2 = Math.Round((-auxB) / 2 / auxA, 2) + " - " + Math.Round(Math.Sqrt(-delta) / 2 / auxA, 2) + " i";
+            // enviar mensagem para o utilizador
+
+
+            // devolver controlo à View
+            return View();
+         }
+
+         // se chegar aqui, alguma coisa correu muito mal...
+         // devolver controlo à View
+         return View();
         }
 
         public IActionResult Privacy() {
